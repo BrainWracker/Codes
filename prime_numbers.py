7eb3a9ee8d2aa86819b5463b4ec03f59d5df0124
BrainKicker <peter.sabanov@gmail.com>
2024-04-09 20:28:00 +0300

    1) Добавил отступы согласно официальным рекомендациям. 2) Добавил точку входа в программу. 3) Сделал вывод более удобным и понятным. 4) Оптимизировал функции findPrimes и sieve (при num=10000 функция findPrimes стала выполняться быстрее примерно в 20 раз, функция sieve - примерно в 400 раз).

--- prime_numbers.py
+++ prime_numbers.py
@@ -1,25 +1,60 @@
 import time
+
+
 def findPrimes(num):
-    a = [2] if num > 2 else []
+
+    if num <= 2:
+        return []
+
+    a = [2]
+
     for n in range(3, num, 2):
-        flag = 1
-        for i in range(3, n//2+1, 2):
-            if n%i == 0:
-                flag = 0
+        isPrime = True
+        for i in range(3, int(num**0.5)+1, 2):
+            if n % i == 0:
+                isPrime = False
                 break
-        if flag:
+        if isPrime:
             a.append(n)
+
     return a
+
 
 def sieve(num):
-    a = list(range(2, num))
-    for x in range(2, int(num**0.5)+1):
-        for y in range(2*x, num, x):
-            if y in a:
-                a.remove(y)
-    return a
 
-start_time = time.time()
-print(findPrimes(100), time.time() - start_time)
-start_time = time.time()
-print(sieve(100), time.time() - start_time)
+    if num <= 2:
+        return []
+    
+    sie = [True] * num
+    sie[0] = sie[1] = False
+    
+    primes = []
+
+    for n in range(2, num):
+        if sie[n] == False:
+            continue;
+        primes.append(n)
+        for m in range(2*n, num, n):
+            sie[m] = False
+
+    return primes
+
+
+if __name__ == '__main__':
+    
+    N = 10000
+
+    start_time = time.time()
+    primes = findPrimes(N)
+    end_time = time.time()
+
+    print('Функция findPrimes.')
+    print('Найденные простые числа:', primes, 'Время (с):', end_time - start_time, sep='\n')
+    print() # new line
+
+    start_time = time.time()
+    primes = sieve(N)
+    end_time = time.time()
+
+    print('Функция sieve.')
+    print('Найденные простые числа:', primes, 'Время (с):', end_time - start_time, sep='\n')
